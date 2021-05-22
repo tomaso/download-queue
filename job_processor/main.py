37fc525ef576e016f395443f54369a04c5a12302
Tomáš Kouba <tomas.kouba@gooddata.com>
2021-05-22 23:31:30 +0200

    Basic job processor implemented.
    The basic job processor downloads files one by one, queue by queue.

---
+++ job_processor/main.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+import django
+import logging
+import os.path
+import requests
+import time
+
+django.setup()
+
+from dq import models
+
+
+def setup_logging() -> None:
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s", level=logging.DEBUG
+    )
+
+
+def setup() -> None:
+    """ 
+    Initial setup of the daemon 
+    """
+    setup_logging()
+
+
+def loop() -> None:
+    """ 
+    TODO: use async methods here and go parallel 
+    """
+    all_jobs = models.DownloadJob.objects.filter(completed__exact=False)
+    if not all_jobs:
+        logging.debug("There is no incomplete job - sleeping for 5s")
+        time.sleep(5)
+    for q in models.Queue.objects.all()[:1]:
+        for dj in q.downloadjob_set.filter(completed__exact=False):
+            logging.debug(
+                f"Queue: {q.name}: Going to download {dj.target_file} from {dj.url} to {dj.target_directory}"
+            )
+            url = dj.url
+            file_name = os.path.join(dj.target_directory, dj.target_file)
+            with open(file_name, "wb") as f:
+                logging.debug(f"{file_name} opened for writing")
+                response = requests.get(url, stream=True)
+                total_length = response.headers.get("content-length")
+                if total_length is None:
+                    f.write(response.content)
+                else:
+                    dl = 0
+                    total_length = int(total_length)
+                    for data in response.iter_content(chunk_size=1024 * 1024):
+                        dl += len(data)
+                        f.write(data)
+                        progress = int(100 * dl / total_length)
+                        dj.progress = progress
+                        dj.save()
+                        logging.debug(f"{dj.target_file}: progress: {dj.progress}%")
+                    dj.completed = True
+                    dj.save()
+
+
+def main() -> None:
+    """
+    TODO: parse arguments
+    """
+    try:
+        setup()
+    except Exception as exc:
+        print(f"Setup of the daemon failed: {exc}")
+    termination_requested = False
+    while not termination_requested:
+        try:
+            loop()
+        except KeyboardInterrupt:
+            logging.debug("Termination requested with Ctrl-c")
+            termination_requested = True
+
+
+if __name__ == "__main__":
+    main()
--- server/dq/models.py
+++ server/dq/models.py
@@ -8,10 +8,11 @@ class Queue(models.Model):
 
 class DownloadJob(models.Model):
     target_directory = models.CharField(max_length=256)
+    target_file = models.CharField(max_length=256, default="file")
     url = models.CharField(max_length=1024)
     queue = models.ForeignKey(Queue, on_delete=models.CASCADE, blank=True, null=True)
     completed = models.BooleanField(default=False)
     priority = models.DecimalField(max_digits=10, decimal_places=0)
-#    progress
+    progress = models.DecimalField(max_digits=3, decimal_places=0, default=0)
 #    start_time
 #    end_time
