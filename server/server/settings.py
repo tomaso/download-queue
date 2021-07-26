c677a5b2f33b7747cb5f511193853813d6992f2d
Tomáš Kouba <tomas.kouba@gooddata.com>
2021-07-26 16:47:42 +0200

    Replace django secret with obviously unused string

--- server/server/settings.py
+++ server/server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-jja&z#3=-ga-+8nrl%mzimdo&1barvyj@-e%9m(+xcqyp=8w+s"
+SECRET_KEY = "REPLACE_ME_DURING_DEPLOYMENT"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
